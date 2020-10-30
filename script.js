23a322c37023c558eb357b6faae49f17e1a54e57
ryslanx <stomatik@ukr.net>
2020-10-31 00:39:29 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,295 @@
+// -- створити об'єкт (не меньше 5ти властивостей) який описує
+// - собаку
+// const dog = {
+//     name: "Edwin",
+//     legs: 4,
+//     age: 1,
+//     isSleeping: false,
+//     breed: "Jack Russel Terrier"
+// }
+
+
+// - людину
+// const person = {
+//     name: "Ruslan",
+//     age: 20,
+//     hasGirlfriend: false,
+//     hobbies: ["programming", "camping", "stretching"],
+//     status: "student"
+// }
+
+
+// - автомобіль
+// const car = {
+//     model: "Volkswagen Passat",
+//     year: 1999,
+//     isNew: false,
+//     mileage: 100000,
+//     tiers: "new"
+// }
+
+
+// - квартиру
+// const flat = {
+//     roomsQuantity: 4,
+//     isBig: true,
+//     address: "Ivana Franka street",
+//     houseNumber: 24,
+//     flatNumber: 18
+// }
+
+
+// - книгу
+// const book = {
+//     title: "Harry Potter and Deathly Hallows",
+//     numberInRow: 8,
+//     pages: 340,
+//     rating: 9,
+//     age: "12+",
+//     category: "fantastic books"
+// }
+
+
+//
+// -- Створити масив та вивести його в консоль:
+//     - з 5 собак
+// let dogs = [{
+//     name: "Edwin",
+//     legs: 4,
+//     age: 1,
+//     isSleeping: false,
+//     breed: "Jack Russel Terrier"
+// },{
+//     name: "Edwin",
+//     legs: 4,
+//     age: 1,
+//     isSleeping: false,
+//     breed: "Jack Russel Terrier"
+// },{
+//     name: "Edwin",
+//     legs: 4,
+//     age: 1,
+//     isSleeping: false,
+//     breed: "Jack Russel Terrier"
+// },{
+//     name: "Edwin",
+//     legs: 4,
+//     age: 1,
+//     isSleeping: false,
+//     breed: "Jack Russel Terrier"
+// },{
+//     name: "Edwin",
+//     legs: 4,
+//     age: 1,
+//     isSleeping: false,
+//     breed: "Jack Russel Terrier"
+// }];
+// console.log(dogs);
+
+
+// - 3 5 людей
+// let people = [
+//     {
+//         name: "Ruslan",
+//         age: 20,
+//         hasGirlfriend: false,
+//         hobbies: ["programming", "camping", "stretching"],
+//         status: "student"
+//     },
+//     {
+//         name: "Ruslan",
+//         age: 20,
+//         hasGirlfriend: false,
+//         hobbies: ["programming", "camping", "stretching"],
+//         status: "student"
+//     },
+//     {
+//         name: "Ruslan",
+//         age: 20,
+//         hasGirlfriend: false,
+//         hobbies: ["programming", "camping", "stretching"],
+//         status: "student"
+//     },
+//     {
+//         name: "Ruslan",
+//         age: 20,
+//         hasGirlfriend: false,
+//         hobbies: ["programming", "camping", "stretching"],
+//         status: "student"
+//     },
+//     {
+//         name: "Ruslan",
+//         age: 20,
+//         hasGirlfriend: false,
+//         hobbies: ["programming", "camping", "stretching"],
+//         status: "student"
+//     }
+// ];
+// console.log(people);
+
+// - з 5 автомобілів
+// let cars = [{
+//     model: "Volkswagen Passat",
+//     year: 1999,
+//     isNew: false,
+//     mileage: 100000,
+//     tiers: "new"
+// }, {
+//     model: "Volkswagen Passat",
+//     year: 1999,
+//     isNew: false,
+//     mileage: 100000,
+//     tiers: "new"
+// },{
+//     model: "Volkswagen Passat",
+//     year: 1999,
+//     isNew: false,
+//     mileage: 100000,
+//     tiers: "new"
+// },{
+//     model: "Volkswagen Passat",
+//     year: 1999,
+//     isNew: false,
+//     mileage: 100000,
+//     tiers: "new"
+// },{
+//     model: "Volkswagen Passat",
+//     year: 1999,
+//     isNew: false,
+//     mileage: 100000,
+//     tiers: "new"
+// }];
+// console.log(cars);
+
+
+// -- створити об'єкт (не меньше 5ти властивостей) який описує, одна з властивостей обов'язково повинна бути об'єктом,ще одна - масивом
+// - будинок
+// const house = {
+//     quantityOfRooms: 4,
+//     kitchen: true,
+//     rooms: ["kitchen", "bathroom", "dining room", "bedroom"],
+//     owner: {
+//         name: "Ruslan",
+//         age: 20,
+//         profit: "1 000 000 $"
+//     },
+//     area: 120
+// }
+
+
+// - водій
+// const driver = {
+//     name: "Ruslan",
+//     age: 20,
+//     cars: ["Wolkswagen Passat", "Bugatti Veyron"],
+//     preferredBeer: {
+//         name: "Lvivske 1715",
+//         preferences: "Dark"
+//     },
+//     drivesFast: true
+// }
+
+
+// - іграшку
+// const toy = {
+//     name: "Bear",
+//     type: "soft",
+//     inside: ["feathers", "money", "marijuana"],
+//     cool: true,
+//     qrCode: {
+//         showAnimation: true,
+//         videoID: 12324324,
+//         attackMode: false
+//     }
+// }
+
+
+// - стіл
+// const table = {
+//     type: "big",
+//     numberOfLegs: 4,
+//     material: ["wood", "plastic", "organic"],
+//     inBuiltUI: {
+//         operatingSystem: "Android",
+//         applications: ["Doodle Jump", "Dota 2", "PokerStars"],
+//     },
+//     color: "black"
+// }
+
+
+// - сумка
+// const bag = {
+//     color: "black",
+//     materials: ["leather", "furs"],
+//     owner: {
+//         name: "Marianna",
+//         age: 20,
+//         job: "customer support"
+//     },
+//     changeable: true,
+//     date: "31.10.2020"
+// }
+
+
+// Дан массив:
+//     let users = [
+//         {name: 'vasya', age: 31, status: false},
+//         {name: 'petya', age: 30, status: true},
+//         {name: 'kolya', age: 29, status: true},
+//         {name: 'olya', age: 28, status: false},
+//         {name: 'max', age: 30, status: true},
+//         {name: 'anya', age: 31, status: false},
+//         {name: 'oleg', age: 28, status: false},
+//         {name: 'andrey', age: 29, status: true},
+//         {name: 'masha', age: 30, status: true},
+//         {name: 'olya', age: 31, status: false},
+//         {name: 'max', age: 31, status: true}
+//     ];
+// - звернутися в відповідну ячейку масиву і відповідний параметр об'єкта і вивести в консольх наступне
+// - статус Андрія
+// console.log(users[7].status);
+// - статус Максима
+// console.log(users[4].status);
+// - ім'я передостаннього об'єкту
+// console.log(users[users.length - 2].name);
+// - ім'я третього об'єкта
+// console.log(users[2].name);
+// - вік Олега
+// console.log(users[6].age);
+// - вік Олі
+// console.log(users[3].age);
+// - вік + ім'я 5го об'єкта
+// console.log(users[4].age + " " + users[4].name);
+// - вік + сатус Анни
+// console.log(users[5].age + " " + users[5].status);
+// Приклад: вивести ім'я 1го об'єкта. Відповідь: console.log (users [0] .name). Будьте уважні! 4й об'єкт має індес 3!
+//
+
+
+// -Візьміть файл template1.html, підключіть до нього скрипт, і працюйте в ньому.
+//
+// -- Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
+// // - отримує текст з параграфа з id "content"
+// let content = document.getElementById("content");
+// console.log(content.textContent);
+// // - отримує текст з блоку з id "rules"
+// let rules = document.getElementById("rules");
+// console.log(rules.textContent);
+// // - замініть текст параграфа з id 'content' на будь-який інший
+// content.textContent = "Hello, my name is Ruslan";
+// // - замініть текст параграфа з id 'rules' на будь-який інший
+// rules.textContent = "I am 20 years old";
+// // - змініть кожному елементу колір фону на червоний
+// content.style.backgroundColor = "red";
+// rules.style.backgroundColor = "red";
+// // - змініть кожному елементу колір тексту на синій
+// content.style.color = "blue";
+// rules.style.color = "blue";
+// // - отримати весь список класів елемента з id=rules і вивести їх в console.log
+// console.log(rules.classList);
+// // - отримати всі елементи з класом fc_rules
+// let fc_rules = document.getElementsByClassName("fc_rules");
+// // - поміняти колір тексту у всіх елементів fc_rules на червоний
+// for (let i = 0; i < fc_rules.length; i++) {
+//     fc_rules[i].style.color = "red";
+// }
